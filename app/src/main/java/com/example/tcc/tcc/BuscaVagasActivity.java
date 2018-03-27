80aebfa3f4041569476a74957dd214490ef992f7
JulianaSatti <31113986+JulianaSatti@users.noreply.github.com>
2018-03-26 22:52:25 -0300

    inclusao tela busca vagas

---
+++ app/src/main/java/com/example/tcc/tcc/BuscaVagasActivity.java
@@ -0,0 +1,13 @@
+package com.example.tcc.tcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuscaVagasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca_vagas);
+    }
+}
