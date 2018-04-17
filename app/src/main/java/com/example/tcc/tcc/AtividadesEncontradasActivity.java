f0fdeb966d265a5efa2d874d27a7a0ddf597952e
JulianaSatti <31113986+JulianaSatti@users.noreply.github.com>
2018-04-16 22:37:05 -0300

    Inclusao de duas telas e pequenas modificicacoes

---
+++ app/src/main/java/com/example/tcc/tcc/AtividadesEncontradasActivity.java
@@ -0,0 +1,13 @@
+package com.example.tcc.tcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtividadesEncontradasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividades_encontradas);
+    }
+}
