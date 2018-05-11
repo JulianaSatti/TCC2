e15a6ec42aff68346ebf960da58a26285124baf8
JulianaSatti <31113986+JulianaSatti@users.noreply.github.com>
2018-05-10 23:14:53 -0300

    Nova tela inicial contendo o navigation drawer. eu tiv que mexer em bastante arquivo pra fazer isso, pode fazer merge com tudo pois ja esta atualizado.

--- app/src/main/java/com/example/tcc/tcc/TelaInicialActivity.java
+++ app/src/main/java/com/example/tcc/tcc/TelaInicialActivity.java
@@ -3,19 +3,19 @@ package com.example.tcc.tcc;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,20 +24,34 @@ import java.net.URL;
 
 import static com.example.tcc.tcc.LoginActivity.enderFoto;
 
-public class TelaInicialActivity extends AppCompatActivity{
+public class TelaInicialActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     private Button ong;
-    private Button doacoes, atividade, logout;
+    private Button doacoes;
+    private Button atividade;
     ImageView foto2;
     Bitmap imgInicial;
     private Handler handler = new Handler();
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { //sobreescrevendo - cria a tela
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_inicial);//acesso a pasta
-////////////////////////////imagem inicial//////////////////////////////////
+        setContentView(R.layout.activity_tela_inicial);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ////////////////////////////imagem inicial//////////////////////////////////
         new Thread(){
             public void run(){
 
@@ -47,8 +61,6 @@ public class TelaInicialActivity extends AppCompatActivity{
                     HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
                     InputStream input = conexao.getInputStream();
                     imgInicial = BitmapFactory.decodeStream(input);
-
-
 
                 }catch (IOException e){
 
@@ -68,21 +80,6 @@ public class TelaInicialActivity extends AppCompatActivity{
             }
         }.start();
 
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-        logout = (Button) findViewById(R.id.logout);//irá voltar para tela de login
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(TelaInicialActivity.this, MainActivity.class));
-            }
-        });
-
-
         doacoes = (Button) findViewById(R.id.btn_doacoes);//aqui que faz transicao de tela ao clique no btn Doações
         doacoes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,7 +97,6 @@ public class TelaInicialActivity extends AppCompatActivity{
             }
         });
 
-        //ira acessar a tela pesquisar VAGA
         atividade = (Button) findViewById(R.id.btn_atividades);
         atividade.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,28 +104,52 @@ public class TelaInicialActivity extends AppCompatActivity{
                 startActivity(new Intent(TelaInicialActivity.this, BuscaAtividadesActivity.class));
             }
         });
-
     }
-
-
 
     @Override
     public void onBackPressed() {
-
-
-
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-
-
-        // não chame o super desse método, assim a tecla voltar fica inutil nesta tela
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        getMenuInflater().inflate(R.menu.perfil, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_close) {
 
+            finish();
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) { //seta o que quer fazer em cada um dos elementos
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.perfil_btn) {
+            // Handle the camera action
+            Intent intent = new Intent(this, VisualizarPerfil.class);
+            startActivity(intent);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
