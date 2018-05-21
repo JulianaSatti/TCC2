e1fe5a464d1de8fd0e8ffc26d75d99a446eb40d2
Aline Silva <linnysilvas@gmail.com>
2018-05-21 15:35:39 -0300

    .

--- app/src/main/java/com/example/tcc/tcc/FacebookActivity.java
+++ app/src/main/java/com/example/tcc/tcc/FacebookActivity.java
@@ -3,6 +3,9 @@ package com.example.tcc.tcc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -57,8 +60,26 @@ public class FacebookActivity extends AppCompatActivity {
         startActivity(facebook);
     }
 
-    public void logout(View view) {
-        LoginManager.getInstance().logOut();
-        goLoginScreen();
+
+    public boolean onCreateOptionsMenu(Menu menu) {// esse metodo que manda na action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sair_google_face, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_close) {
+            LoginManager.getInstance().logOut();
+            goLoginScreen();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
